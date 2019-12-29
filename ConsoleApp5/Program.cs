86cd30f12b81d3c46957ded5320a6431f2e74438
mammadroid <mammadroid@DESKTOP-I6OEJ2J>
2019-12-30 02:23:01 +0330

    first commit

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        unsafe static void Main(params string[] args)
+        {
+            A<int> test = new A<int>(10);
+            for (int i = 0; i < test.arr.Length; i++)
+            {
+                test.arr[i] = (i + 1) * 10;
+            }
+            
+            foreach(int element in test)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+    static class initializer
+    {
+        public static void printVal<T>(this T value) => Console.WriteLine($"type: {typeof(T)}\t{value}");
+    }
+    class A<T> : IEnumerable<T>
+    {
+        private T[] Arr;
+        public A(int sizeOf)
+        {
+            Arr = new T[sizeOf];
+        }
+        
+        public T[] arr
+        {
+            get
+            {
+                return Arr;
+            }
+            set
+            {
+                Arr = value;
+            }
+        }
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                yield return arr[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                yield return arr[i];
+            }
+        }
+        
+    }
+    
+
+
+}
